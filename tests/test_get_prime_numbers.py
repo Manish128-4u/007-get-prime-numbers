b8118acae2ad14e84cb42dbb58da33ace46c56fd
Manish128-4u <53977826+Manish128-4u@users.noreply.github.com>
2019-10-22 23:22:31 +0530

    test prime

--- tests/test_get_prime_numbers.py
+++ tests/test_get_prime_numbers.py
@@ -1,20 +1,16 @@
-from unittest import TestCase
-from errors import incorrect_output, no_function_found, succeed
-
-
-class TestGet_prime_numbers(TestCase):
-    def test_prime_numbers(self):
-        try:
-            from prime_numbers import get_prime_numbers
-        except ImportError:
-            self.assertFalse(no_function_found("get_prime_numbers"))
-
-        result = get_prime_numbers(10)
-        try:
-            self.assertEqual(2, result[0])
-            self.assertEqual(3, result[1])
-            self.assertEqual(5, result[2])
-            self.assertEqual(7, result[3])
-            self.assertTrue(succeed("Test cases passed"))
-        except:
-            self.assertFalse(incorrect_output("function should return array of prime numbers"))
+def enter_int(num):
+    primes=[2]
+    m=3
+    if num<2:
+        return 0
+    while m<=num:
+        for n in primes:
+            if m%n==0:
+                m+=2
+                break
+         else:
+            primes.append(m)
+            m+=2
+            print(primes)
+            return len(primes)
+                    
